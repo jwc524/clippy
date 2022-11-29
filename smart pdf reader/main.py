10d971f0c01df779cb0f6d1bb227d52d07e33339
ryanctruong <114968495+ryanctruong@users.noreply.github.com>
2022-11-29 14:50:20 -0600

    Update main.py

--- smart pdf reader/main.py
+++ smart pdf reader/main.py
@@ -1,11 +1,34 @@
-import pdfplumber
 
-file_name = 'example.pdf'
+import os
+from tkinter import *
+from tkinter import filedialog
+from tkPDFViewer import tkPDFViewer as pdf
 
-with pdfplumber.open(file_name) as pdf:
-    page = pdf.pages[0]
-    print(page.extract_text())
+root = Tk()
+root.geometry("630x700+400+100")
+root.title("PDF viewer")
+root.configure(bg="white")
 
+
+def browseFiles():
+    filename = filedialog.askopenfilename(initialdir=os.getcwd(),
+                                          title="select pdf file",
+                                          filetypes=(("PDF File", ".pdf"),
+                                                     ("PDF File", ".PDF"),
+                                                     ("All file", ".txt")))
+
+    v1 = pdf.ShowPdf()
+    v2 = v1.pdf_view(root, pdf_location=open(filename, "r"), width=77, height=100)
+    v2.pack(pady=(0, 0))
+
+
+Button(root, text="open", command=browseFiles, width=40,
+       font="arial 20", bd=4).pack()
+root.mainloop()
+
+
+
+  #ORIGINAL
 # import pdfreader
 # from pdfreader import PDFDocument, SimplePDFViewer
 
