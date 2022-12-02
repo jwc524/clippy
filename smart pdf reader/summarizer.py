2df815f01abc63b89911d176bc32663c689ad843
tony-tomass <114968472+tony-tomass@users.noreply.github.com>
2022-12-01 22:42:30 -0600

    Update summarizer.py
    Added some minor updates to functions and reworded comments to make more sense

--- smart pdf reader/summarizer.py
+++ smart pdf reader/summarizer.py
@@ -2,44 +2,49 @@ import os
 import pdfplumber
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
+from string import punctuation 
 
 #replace 'example.pdf' with name of another file
 with pdfplumber.open(r'example.pdf') as pdf:
     extracted_page = pdf.pages[0]
-    extracted_text = extracted_page.extract_text()
-
+    extracted_text = extracted_page.extract_text(x_tolerance = 1)      # x_tolerance is default to 3; changing to one removes tendency for it to group together
+                                                                       # multiple words due to a new line
 def summarize(text):
-    stopword = set(stopwords.words("english"))                   # Removes stopwords (a, the, and, in, etc.)
-    words = word_tokenize(text)                         # Word tokenizer for tokenizing
-    freq_tabl = dict()                                            # Frequency table uses dictionary to track most commonly used words excluding all stopwords
-    for word in words:                                            
+    summary = ''                                                  
+    stopword = set(stopwords.words("english"))                         # Removes stopwords (a, the, and, in, etc.); separate one for puncuation also
+    words = word_tokenize(text)                                        
+    freq_tabl = dict()                                                 # Basically counts number of times a word is used in text
+    for word in words:                                                 
         word = word.lower()
         if word in stopword:
+            continue
+        if word in punctuation:
             continue
         if word in freq_tabl:
             freq_tabl[word] += 1
         else:
             freq_tabl[word] = 1
 
-    sentences = sent_tokenize(text)                     # Functions like word tokenizer, but now applies for full sentences
-    sent_tabl = dict()                                            # Similar to frequency table
-    for sentence in sentences:
+    sentences = sent_tokenize(text)                               
+    sent_tabl = dict()                                            # Gives a sentence a score based on the combined scores of words
+    for sentence in sentences:                                    # Will also account for anytime a word/part of a word shows up in another sentence
         for word, freq in freq_tabl.items():
+            if word in punctuation:
+                continue
             if word in sentence.lower():
                 if sentence in sent_tabl:
                     sent_tabl[sentence] += freq
                 else:
-                    sent_tabl[sentence] = freq
+                    sent_tabl[sentence] = freq 
 
-    sum = 0                                                       # This and the following for loop are used to score sentences based on a perceived
-    for sentence in sent_tabl:                                    # importance that comes from how frequently the words in the sentence pops up elsewhere
+    sum = 0                                                       # Sum = total added up sentence scores
+    for sentence in sent_tabl:      
         sum += sent_tabl[sentence]
 
-    average = int(sum / len(sent_tabl))                             # Average score of a sentence is used to determine what is used in the summary
-    summary = ''
-    for sentence in sentences:
-        if (sentence in sent_tabl) and (sent_tabl[sentence] > (1.2 * average)):
-            summary += " " + sentence
+    average = int(sum / len(sent_tabl))                                              # Gets average sentence score
+    for sentence in sentences:                                                       # Determines what will be put into the summary; above average = more importance
+        if (sentence in sent_tabl) and (sent_tabl[sentence] > (1.4625 * average)):   # Changing the value multiplied to average will narrow down the summary
+            summary += " " + sentence                                                
     
     return summary
 
@@ -51,12 +56,21 @@ def common_words(text):
         word = word.lower()
         if word in stopword:
             continue
+        if word in punctuation:
+            continue
         if word in freq_tabl:
             freq_tabl[word] += 1
         else:
             freq_tabl[word] = 1
 
-    return freq_tabl
+    sorted_tabl = dict(sorted(freq_tabl.items(), key = lambda item: item[1], reverse = True))
+    return sorted_tabl
 
+    #return freq_tabl
+
+#print(extracted_text)
+#print("\n     Summary:")
 #print(summarize(extracted_text))
+#print("\nCommon words:")
 #print(common_words(extracted_text))
+#print(punctuation)
