682caa35ba8ee625801281c8fe99fb4af8b18474
ryanctruong <114968495+ryanctruong@users.noreply.github.com>
2022-12-01 21:22:14 -0600

    Update glitter.py

--- smart pdf reader/glitter.py
+++ smart pdf reader/glitter.py
@@ -1,8 +1,83 @@
+# import camelot.io as camelot
+# # from tabula.io import read_pdf
+#
+# # extract all the tables in the PDF file
+# abc = camelot.read_pdf("BURT.pdf")  # address of file location
+#
+# # print the first table as Pandas DataFrame
+# print(abc[0].df)
+#
+# # from pdfreader import PDFDocument, SimplePDFViewer
+# #
+# # file_name = "BURT.pdf"
+# #
+# # fd = open(file_name, "rb")
+# # viewer = SimplePDFViewer(fd)
+# #
+# # for canvas in viewer:
+# #     page_img = canvas.images
+# #     page_forms = canvas.forms
+# #     page_txt = canvas.text_content
+# #     page_inline_img = canvas.inline_images
+# #     page_strings = canvas.strings
+# #
+# # viewer.navigate(1)
+# # viewer.render()
+# # print(viewer.canvas.strings)
+# # print(viewer.canvas.text_content)
+# #
+# # import pdfplumber
+# #
+# # file_name = 'BURT.pdf'
+# #
+# # with pdfplumber.open(file_name) as pdf:
+# #     page = pdf.pages[12]
+# #     print(page.extract_text())
+#
+#
+#
+#
+# # # STEP 1
+# # # import libraries
+# # import fitz
+# # import io
+# # from PIL import Image
+# #
+# # # STEP 2
+# # # file path you want to extract images from
+# # file = "BURT.pdf"
+# #
+# # # open the file
+# # pdf_file = fitz.open(file)
+# #
+# # # STEP 3
+# # # iterate over PDF pages
+# # for page_index in range(len(pdf_file)):
+# #
+# #     # get the page itself
+# #     page = pdf_file[page_index]
+# #     image_list = page.getImageList()
+# #
+# #     # printing number of images found in this page
+# #     if image_list:
+# #         print(f"[+] Found a total of {len(image_list)} images in page {page_index}")
+# #     else:
+# #         print("[!] No images found on page", page_index)
+# #     for image_index, img in enumerate(page.getImageList(), start=1):
+# #         # get the XREF of the image
+# #         xref = img[0]
+# #
+# #         # extract the image bytes
+# #         base_image = pdf_file.extractImage(xref)
+# #         image_bytes = base_image["image"]
+# #
+# #         # get the image extension
+# #         image_ext = base_image["ext"]
 import os
 from tkinter import *
 from tkinter import filedialog
 import tkinter as tk
-
+import subprocess
 import PyPDF2
 import fpdf
 from PyPDF2 import PdfFileWriter, PdfFileReader
@@ -76,7 +151,7 @@ def new_page():
     pdfs = ['heading.pdf', file]
 
     # output pdf file name
-    output = 'new_file.pdf'
+    output = 'merged.pdf'
 
     # calling pdf merge function
     PDFmerge(pdfs=pdfs, output=output)
@@ -88,42 +163,52 @@ def clicked():
         names_label = tk.Label(top_window)
         names_label.grid(row=int(ind) + 1, column=0)
         names_label.config(text=h)
+    subprocess.Popen(['links.pdf'], shell=True)
+
+
+def links():
+    pdf_writer = PdfFileWriter()
+    pdf_reader = PdfFileReader(open('merged.pdf', 'rb'))
+
+    num_of_pages = pdf_reader.getNumPages()
+
+    for page in range(num_of_pages):
+        current_page = pdf_reader.getPage(page)
+        pdf_writer.addPage(current_page)
+
+    # dimensions is 612x792
+    # Abstract to Question for Detection
+    pdf_writer.addLink(pagenum=0, pagedest=1, rect=RectangleObject([20, 715, 200, 765]), )
+
+    # Search for Answer to Nodejs Glitter Data
+    pdf_writer.addLink(pagenum=0, pagedest=2, rect=RectangleObject([20, 655, 200, 713]), )
+
+    # Classifier to Results
+    pdf_writer.addLink(pagenum=0, pagedest=3, rect=RectangleObject([20, 615, 200, 653]), )
+
+    # Related Works to Conclusion and Future Work
+    pdf_writer.addLink(pagenum=0, pagedest=4, rect=RectangleObject([20, 540, 200, 613]), )
+
+    # References
+    pdf_writer.addLink(pagenum=0, pagedest=5, rect=RectangleObject([20, 520, 200, 538]), )
+
+    # Figure One
+    pdf_writer.addLink(pagenum=1, pagedest=2, rect=RectangleObject([407, 194, 417, 205]), )
+
+    with open(os.path.abspath('links.pdf'), 'wb') as link_pdf:
+        pdf_writer.write(link_pdf)
 
 
 btn = tk.Button(window, text="Print Headings", command=clicked)
 btn.grid(column=0, row=0, padx=30, pady=2)
 
-new_page()
 
-pdf_writer = PdfFileWriter()
-pdf_reader = PdfFileReader(open('new_file.pdf', 'rb'))
+def main():
+    new_page()
+    links()
 
-num_of_pages = pdf_reader.getNumPages()
+    root.mainloop()
 
-for page in range(num_of_pages):
-    current_page = pdf_reader.getPage(page)
-    pdf_writer.addPage(current_page)
 
-# dimensions is 612x792
-# Abstract to Question for Detection
-pdf_writer.addLink(pagenum=0, pagedest=1, rect=RectangleObject([20, 715, 200, 765]), )
-
-# Search for Answer to Nodejs Glitter Data
-pdf_writer.addLink(pagenum=0, pagedest=2, rect=RectangleObject([20, 655, 200, 713]), )
-
-# Classifier to Results
-pdf_writer.addLink(pagenum=0, pagedest=3, rect=RectangleObject([20, 615, 200, 653]), )
-
-# Related Works to Conclusion and Future Work
-pdf_writer.addLink(pagenum=0, pagedest=4, rect=RectangleObject([20, 540, 200, 613]), )
-
-# References
-pdf_writer.addLink(pagenum=0, pagedest=5, rect=RectangleObject([20, 520, 200, 538]), )
-
-# Figure One
-pdf_writer.addLink(pagenum=1, pagedest=2, rect=RectangleObject([407, 194, 417, 205]), )
-
-with open(os.path.abspath('new_file_1.pdf'), 'wb') as link_pdf:
-    pdf_writer.write(link_pdf)
-
-root.mainloop()
+if __name__ == '__main__':
+    main()
